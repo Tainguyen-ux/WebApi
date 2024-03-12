c17ed6306eb056f6dac28aee901f88ed94cb6dc0
Tainguyen-ux <140154565+Tainguyen-ux@users.noreply.github.com>
2024-03-12 20:56:26 +0700

    Getting Started

---
+++ 01-GettingStarted/01-GettingStarted/Controllers/ProductController.cs
@@ -0,0 +1,86 @@
+﻿using _01_GettingStarted.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _01_GettingStarted.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        public static List<Product> products = new List<Product>();
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(products);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetId(string id)
+        {
+            try
+            {
+                var product = products.SingleOrDefault(x => x.ID == Guid.Parse(id));
+                if (product == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return Ok(product);
+                }
+            }
+            catch
+            {
+                return BadRequest();
+            }
+            
+        }
+
+        [HttpPost]
+        public IActionResult Create(ProductVM productVM) 
+        {
+            var product=new Product()
+            {
+                ID=Guid.NewGuid(),
+                Name=productVM.Name,
+                Price=productVM.Price,
+            };
+            products.Add(product);
+            return Ok(new
+            {
+                Code=200,
+                Id=product.ID,
+            });
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Edit(string id, Product product) 
+        {
+            try
+            {
+                var producation = products.SingleOrDefault(x=>x.ID == Guid.Parse(id));
+                if(producation == null)
+                {
+                    return NotFound(new
+                    {
+                        Code=404,
+                        Message="Không tìm thấy dữ liệu"
+                    });
+                }
+                else
+                {
+                    producation.Name= product.Name;
+                    producation.Price= product.Price;
+
+                    return Ok(producation);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            
+        }
+    }
+}
---
+++ 01-GettingStarted/01-GettingStarted/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace _01_GettingStarted.Models
+{
+    public class ProductVM
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+    }
+    public class Product:ProductVM
+    {
+        public Guid ID { get; set; }
+
+    }
+}
